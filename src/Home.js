b48c874af620b69fefca46f2d647b4948d73d771
Jackson <jackson.bounds@gmail.com>
2023-08-09 18:32:28 -0400

    React tutorial 7 complete

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-    </div>
-  );
+
+    const handleClick = (e) => {
+        console.log("Hello!");
+    }
+
+    const handleClickString = (e, name) => {
+        console.log(e.screenX, name)
+    }
+
+	return (
+		<div className="home">
+			<h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickString(e,"YOOOO!!!")}>Click me too!</button>
+		</div>
+	);
 }
- 
+
 export default Home;
